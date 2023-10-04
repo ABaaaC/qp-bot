8f516386def0d9a1dd905efc90e3a948401d5004
Stanislav <rus.abaaac@gmail.com>
2023-10-04 13:17:02 +0300

    fixed lottery number picture

--- src/telebot10_aio.py
+++ src/telebot10_aio.py
@@ -68,13 +68,15 @@ async def start(message: Message, state: FSMContext) -> None:
 
     # # generate file_id's
     # file_id_dict = {}
-    # for i in range(1, 4):
+    # for i in range(1, 400):
     #     screenshot_path = os.getcwd() + '/' + f"screenshots/lottery_number_{i}.png"
     #     photo = FSInputFile(path=screenshot_path)
     #     sent_message = await message.answer_photo(photo=photo)
     #     file_id = sent_message.photo[-1].file_id
     #     file_id_dict[i] = file_id
     #     await sent_message.delete()
+    # with open("file_id_dict.json", "w") as outfile:
+    #     json.dump(file_id_dict, outfile, indent=4)
 
     logger.info("Start is really calling")
     city = "moscow"
